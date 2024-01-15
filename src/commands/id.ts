20c3abf789368dd2a841e44fef1556d22df9ca63
Wds Utilidades <113811955+WdsUtilidades@users.noreply.github.com>
2024-01-15 20:07:50 -0300

    Update id.ts

--- src/commands/id.ts
+++ src/commands/id.ts
@@ -1,7 +1,7 @@
 import { Telegraf, ContextMessageUpdate } from 'telegraf'
 
 export function setupId(bot: Telegraf<ContextMessageUpdate>) {
-  bot.command('id', async (ctx) => {
+  bot.command('idoff777', async (ctx) => {
     return ctx.reply(`${ctx.chat.id}, ${ctx.from.id}`)
   })
 }
